7a9d81b46826d16bd0e7f10bc5efee466fcd5b6f
Muhammad Talha <talhaguy@gmail.com>
2022-01-16 22:16:46 -0800

    rm sanity

--- src/sanity/content/queries.ts
+++
@@ -1,7 +0,0 @@
-export const ALL_CONTENT_SLUGS = `*[_type == "contentPage"].slug.current`;
-
-export const FETCH_CONTENT_BY_SLUG_QUERY = `*[_type == "contentPage" && slug.current == $slug][0]{
-  name,
-  "heroImage": image.asset->url,
-  content
-}`;
--- src/sanity/index.ts
+++
@@ -1,9 +0,0 @@
-export { fetchAllProductSlugs, fetchProductBySlug } from "./product/factories";
-export {
-    fetchAllCategorySlugs,
-    fetchCategoryBySlug,
-} from "./category/factories";
-export { fetchAllContentSlugs, fetchContentBySlug } from "./content/factories";
-export { fetchLandingPageDataBySlug } from "./landing/landing";
-
-export { fetchNavigationLinks } from "./navigation/factories";
